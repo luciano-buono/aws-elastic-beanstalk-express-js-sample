fff9da441447731fac276a065977440a826e60cd
luciano-buono <luciano@DESKTOP-Lucho.localdomain>
2021-05-06 13:34:37 -0300

    change message v3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Nasini Version2!'));
+app.get('/', (req, res) => res.send('Hello Nasini Version2\n TEST!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
